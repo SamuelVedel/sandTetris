2df92be52a9faf4c75e4b0f11c3fba80e29f6690
VEDEL Samuel <Samuel.Vedel@bordeaux-inp.fr>
2024-07-02 19:00:27 +0200

    [Feat](menu)<SettingsMenu.java> the begging of the class SettingsMenu

---
+++ src/fr/svedel/sandtetris/SettingsMenu.java
@@ -0,0 +1,19 @@
+package fr.svedel.sandtetris;
+
+import java.awt.Color;
+
+import fr.svedel.vcomponent.VButton;
+import fr.svedel.vcomponent.VPanel;
+
+public class SettingsMenu extends Menu {
+	
+	private static final int PANEL_GAP = 5;
+	private static final int PANEL_WIDTH = 15;
+	private static final Color PANLE_COLOR = Color.BLACK.brighter();
+	
+	private VPanel 
+	
+	public SettingsMenu() {
+		super(800, 600);
+	}
+}
